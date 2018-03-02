5062a761898aed4412eb1314d60806128b0e8605
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-03-02 14:07:52 -0500

    q and a are working, hiding/showing is working, but timer still not stopping

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,70 @@
-
-/*	- create a timer function (use solved timer from class) that resets for each question
-	- hide() all questions until one before is answered
-	- array for right answers, wrong answers, unanswered, then arr.length to tell user
-	- show correct answer when wrong - maybe show() with .class attribute added?
-	- questions for each written in html with hide() and show() methods
-	- onclick events to start, next question, timer function
-	- timer - next question if time runs out, onclick in .answers class disables timer, then goes straight to next question
-	- reset game button at end
-	- buttons for answers? .class added to each correct answer, useful for detecting correct answer click & displaying correct .class when ans is wrong?
-*/
-
 $(document).ready(function() {
 
-var triviaItems = [".one .two", ".three", ".four", ".five", ".six", ".seven", ".eight"];
-var correctGuesses = 0;
-var wrongGuesses = 0;
-var totalAnswered = 0;
+	var triviaItems = ["one", "two", "three", "four", "five", "six", "seven", "eight", "end"];
+	var correctGuesses = 0;
+	var wrongGuesses = 0;
+	var totalAnswered = 0;
 
+	$(".one, .two, .three, .four, .five, .six, .seven, .eight").hide();
+	$(".answers, .questions").hide();
+	$("#start").show();
 
-$(".one, .two, .three, .four, .five, .six, .seven, .eight").hide();
-$(".answers, .questions").hide();
-$("#start").show();
 
 // clicking on the start button begins the game by showing the first question
 $("#start").on("click", function() {
 	$("#start").hide();
 	$("h1").hide();
 	$(".questions, .answers, .one").show();
-	setTimeout(showAnswer, 1000 * 15);
+	var timer = setTimeout(showAnswer, 1000 * 15);
 });
 
 // clicking on wrong answer turns all wrongs red and shows correct in green
-$(".wrong").click(function() {
-  clearTimeout();
-  $(".wrong").addClass("wrongboo");
-  $(".correct").addClass("correctyay");
-  wrongGuesses++;
-  totalAnswered++;
-  setTimeout(nextQuestion, 1000 * 3);
+$(".wrong").on("click", function(timer) {
+	clearTimeout(timer);
+	$(".wrong").addClass("wrongboo");
+	$(".correct").addClass("correctyay");
+	wrongGuesses++;
+	totalAnswered++;
+	$(".wrong").fadeOut();
+	setTimeout(next, 1000 * 1);
 });
 
 // clicking on correct answer turns it green
-$(".correct").click(function() {
-	clearTimeout();
+$(".correct").on("click", function(timer) {
+	clearTimeout(timer);
 	$(".correct").addClass("correctyay");
 	correctGuesses++;
 	totalAnswered++;
-	setTimeout(next, 1000 * 3);
+	$(".wrong").fadeOut();
+	setTimeout(next, 1000 * 1);
 });
 
-// showing correct answer in green if question os not answered in time
-function showAnswer() {
+// showing correct answer in green if question is not answered in time
+function showAnswer(timer) {
+	clearTimeout(timer);
 	$(".correct").addClass("correctyay");
-
+	$(".wrong").hide();
+	totalAnswered++;
+	setTimeout(next, 1000 * 1);
 }
-
-function next() {
-	var c = triviaItems[totalAnswered];
-	nextQuestion(".questions", ".answers", c);
-	}
 
 // on to the next question
-function nextQuestion(a, b, c) {
-	$(a, b, c).show();
+function next(timer) {
+	clearTimeout(timer);
+//	$(".questions", ".answers").hide();
+	$(".answers").removeClass("wrongboo");
+	$(".answers").removeClass("correctyay");
+	console.log(triviaItems[totalAnswered]);
+	var currentQuestion = document.getElementsByClassName(triviaItems[totalAnswered]);
+	var previousQuestion = document.getElementsByClassName(triviaItems[totalAnswered - 1]);
+	$(previousQuestion).remove();
+	$(currentQuestion).removeClass("wrongboo correctyay");
+	$(currentQuestion).show();
+	if (triviaItems[totalAnswered] === "end") {
+	$(".end-screen").addClass("end");
+	$(".end-screen").append("You got " + correctGuesses + " of them right, and " + wrongGuesses + " of them wrong!");
 }
+}
+
 
 });
